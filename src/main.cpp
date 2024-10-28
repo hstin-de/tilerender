e79b63a7d956ef5a1073cdefd66b7169b3d15ee2
philipredstone <rothstein@prtex.de>
2024-10-28 13:23:47 +0000

    improved the cli usage

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,14 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <sqlite3.h>
+#include <getopt.h>
+#include <filesystem>
 
 #include "image_encoding.hpp"
 #include "mbtiles.hpp"
 #include "coordinates.hpp"
+
+namespace fs = std::filesystem;
 
 void renderTiles(int processId, int numProcesses, int maxZoom, const char *style_url, ImageFormat imageFormat, const char *dbPath)
 {
@@ -25,8 +29,6 @@ void renderTiles(int processId, int numProcesses, int maxZoom, const char *style
     uint32_t height = 512;
 
     using namespace mbgl;
-
-    auto mapTilerConfiguration = TileServerOptions::MapTilerConfiguration();
 
     util::RunLoop loop;
 
@@ -42,8 +44,7 @@ void renderTiles(int processId, int numProcesses, int maxZoom, const char *style
             .withCachePath("")
             .withMaximumCacheSize(0)
             .withAssetPath("")
-            .withApiKey("")
-            .withTileServerOptions(mapTilerConfiguration));
+            .withApiKey(""));
 
     map.getStyle().loadURL(style_url);
 
@@ -140,6 +141,20 @@ void renderTiles(int processId, int numProcesses, int maxZoom, const char *style
     sqlite3_close(db);
 }
 
+void printHelp(const char *programName)
+{
+    std::cout << "Usage: " << programName << " [options]\n\n"
+              << "Options:\n"
+              << "  -s, --style <style_url>         URL of the style to use, can be a local file or a remote URL (required!)\n"
+              << "  -z, --zoom <maxZoom>            Maximum zoom level (integer)\n"
+              << "  -p, --processes <numProcesses>  Number of parallel processes (integer)\n"
+              << "  -o, --output <outputDbPath>     Path to the output database\n"
+              << "  -f, --format <imageFormat>      Image format: 'webp', 'jpg', or 'png'\n"
+              << "  -h, --help                      Display this help message\n\n"
+              << "Example:\n"
+              << "  " << programName << " -s https://demotiles.maplibre.org/style.json -z 6 -p 24 -o demotiles.mbtiles -f webp\n";
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -148,12 +163,23 @@ int main(int argc, char *argv[])
     std::string style_url;
     int maxZoom = 5;
     int numProcesses = std::thread::hardware_concurrency();
-    std::string outputDbPath = "./tiles.mbtiles";
+    if (numProcesses == 0) numProcesses = 1; // fallback to 1 process if hardware_concurrency() returns 0
+    std::string outputPath = "./tiles.mbtiles";
     ImageFormat imageFormat = ImageFormat::WEBP;
 
     // Command-line options parsing
+    static struct option long_options[] = {
+        {"style", required_argument, nullptr, 's'},
+        {"zoom", required_argument, nullptr, 'z'},
+        {"processes", required_argument, nullptr, 'p'},
+        {"output", required_argument, nullptr, 'o'},
+        {"format", required_argument, nullptr, 'f'},
+        {"help", no_argument, nullptr, 'h'},
+        {nullptr, 0, nullptr, 0}};
+
     int opt;
-    while ((opt = getopt(argc, argv, "s:z:p:o:f:")) != -1)
+    // Parse command-line options
+    while ((opt = getopt_long(argc, argv, "s:z:p:o:f:h", long_options, nullptr)) != -1)
     {
         switch (opt)
         {
@@ -161,43 +187,86 @@ int main(int argc, char *argv[])
             style_url = optarg;
             break;
         case 'z':
-            maxZoom = std::stoi(optarg);
+            try
+            {
+                maxZoom = std::stoi(optarg);
+                if (maxZoom < 0 || maxZoom > 22)
+                {
+                    throw std::out_of_range("Zoom level must be between 0 and 22.");
+                }
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << "Error: Invalid zoom level. " << e.what() << "\n";
+                return EXIT_FAILURE;
+            }
             break;
         case 'p':
-            numProcesses = std::stoi(optarg);
+            try
+            {
+                int temp = std::stoi(optarg);
+                if (temp > 0)
+                {
+                    numProcesses = static_cast<unsigned int>(temp);
+                }
+            }
+            catch (const std::exception &e)
+            {
+                std::cerr << "Error: Invalid number of processes. " << e.what() << "\n";
+                return EXIT_FAILURE;
+            }
             break;
         case 'o':
-            outputDbPath = optarg;
+            outputPath = optarg;
             break;
         case 'f':
-            if (std::string(optarg) == "webp")
+        {
+            std::string formatStr = optarg;
+            // Convert to lowercase for case-insensitive comparison
+            std::transform(formatStr.begin(), formatStr.end(), formatStr.begin(),
+                           [](unsigned char c)
+                           { return std::tolower(c); });
+
+            if (formatStr == "webp")
             {
                 imageFormat = ImageFormat::WEBP;
             }
-            else if (std::string(optarg) == "jpg")
+            else if (formatStr == "jpg" || formatStr == "jpeg")
             {
                 imageFormat = ImageFormat::JPEG;
             }
-            else if (std::string(optarg) == "png")
+            else if (formatStr == "png")
             {
                 imageFormat = ImageFormat::PNG;
             }
             else
             {
-                std::cerr << "Error: invalid image format specified, must be one of 'webp', 'jpg', or 'png'\n";
-                return 1;
+                std::cerr << "Error: Invalid image format specified. Choose 'webp', 'jpg', or 'png'.\n";
+                return EXIT_FAILURE;
             }
             break;
+        }
+        case 'h':
+            printHelp(argv[0]);
+            return EXIT_SUCCESS;
         default:
-            std::cerr << "Usage: " << argv[0] << " -s style_url [-z maxZoom] [-p numProcesses] [-o outputDbPath] [-f imageFormat]\n";
-            return 1;
+            printHelp(argv[0]);
+            return EXIT_FAILURE;
         }
     }
 
     if (style_url.empty())
     {
-        std::cerr << "Error: style URL must be specified with -s\n";
-        return 1;
+        std::cerr << "Error: Style URL is required.\n\n";
+        printHelp(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if (fs::exists(outputPath))
+    {
+        std::cerr << "Error: Output file '" << outputPath << "' already exists.\nPlease choose a different output path or remove the existing file before proceeding.\n\n";
+        printHelp(argv[0]);
+        return EXIT_FAILURE;
     }
 
     if (style_url.find("http://") != 0 && style_url.find("https://") != 0 && style_url.find("file://") != 0)
@@ -210,11 +279,11 @@ int main(int argc, char *argv[])
     std::cout << "Max Zoom: " << maxZoom << std::endl;
     std::cout << "Number of Processes: " << numProcesses << std::endl;
     std::cout << "Image Format: " << imageString(imageFormat) << std::endl;
-    std::cout << "Output Database: " << outputDbPath << std::endl;
+    std::cout << "Output Path: " << outputPath << std::endl;
     std::cout << "===================================" << std::endl
               << std::endl;
 
-    std::cout << ">>> Starting Rendering" << std::endl;
+    std::cout << ">>> Starting Rendering..." << std::endl;
 
     std::vector<pid_t> pids;
     std::vector<std::string> dbPaths;
@@ -255,8 +324,8 @@ int main(int argc, char *argv[])
     std::chrono::duration<double> elapsedTime = endTime - startTime;
     std::cout << ">>> Finished Rendering in " << elapsedTime.count() << " seconds." << std::endl;
 
-    createMBTilesDatabase(outputDbPath.c_str(), imageFormat);
-    mergeMBTiles(dbPaths, outputDbPath.c_str());
+    createMBTilesDatabase(outputPath.c_str(), imageFormat);
+    mergeMBTiles(dbPaths, outputPath.c_str());
 
     for (const auto &dbPath : dbPaths)
     {
